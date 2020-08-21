298b70fa9003879a0a95139e411ff7d146ee82fa
tomgalpin <tomgalpin@gmail.com>
2020-08-21 11:24:50 -0400

    add GA

--- src/main.js
+++ src/main.js
@@ -2,11 +2,18 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+import VueAnalytics from 'vue-analytics';
 
 // Styles:
 import './assets/styles/main.scss';
 
 Vue.config.productionTip = false;
+
+Vue.use(VueAnalytics, {
+  id: 'UA-40101532-3',
+  checkDuplicatedScript: true,
+  router
+});
 
 new Vue({
   router,
